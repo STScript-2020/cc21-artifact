5bb8357c53bda4dfb7bb3833c641a60e196fcd25
Anson Miu <me@ansonmiu.dev>
2020-12-29 21:00:45 +0000

    Fix run_benchmark script

--- perf-benchmarks/run_benchmark.sh
+++ perf-benchmarks/run_benchmark.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
+if [ "$#" -eq  "0" ]
+then
+    ARGS=(-m 100 1000 -r 20)
+else
+    ARGS=("$@")
+fi
+
 cd ~/perf-benchmarks/simple_pingpong
-python3.8 scripts/all.py -m 100 1000 -r 20
+python3.8 scripts/all.py "${ARGS[@]}"
 
 cd ../complex_pingpong
-python3.8 scripts/all.py -m 100 1000 -r 20
+python3.8 scripts/all.py "${ARGS[@]}"
